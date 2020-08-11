93a1366cffbb8aecbb055dc483c7896665210515
AmirWayne <Amirwayne95@gmail.com>
2020-08-11 17:41:21 +0300

    Navigation Slide Added

--- src/Controller/PatientPaneController.java
+++ src/Controller/PatientPaneController.java
@@ -97,7 +97,6 @@ public class PatientPaneController implements Initializable {
         list.addAll(a,b);
         ChoicePatient.setValue("Choose Patient");
         ChoicePatient.getItems().addAll(list);
-
     }
 
 
